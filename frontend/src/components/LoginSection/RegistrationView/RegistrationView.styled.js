14c0410322b0e247956e067dc29b472773150dcc
Sıbel GAROFALO <sblgrfl@gmail.com>
2022-10-26 18:00:25 +0200

    Login, Validation and Registration

--- frontend/src/components/LoginSection/RegistrationView/RegistrationView.styled.js
+++ frontend/src/components/LoginSection/RegistrationView/RegistrationView.styled.js
@@ -2,5 +2,19 @@ import styled from "styled-components";
 
 
 export const RegistrationViewContainer = styled.div`
-background: white;
+display: flex;
+flex-direction: column;
+height: 100vh;
+align-items: center;
+background-color: #F2F2F2;
+`;
+
+export const RegistrationViewForm = styled.form`
+width: 300px;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+gap: 12px;
+margin-top: 48px;
 `;
